bc6c62010a4bb37f9e7852e954f886b88034b40d
JoonYoop Volt <ratchanon_01@hotmail.com>
2022-02-15 10:27:33 +0700

    Edit

--- Com_AdminCutdoc/EditCutdoc.Designer.cs
+++ Com_AdminCutdoc/EditCutdoc.Designer.cs
@@ -34,6 +34,8 @@
             this.btnSync = new DevExpress.XtraEditors.SimpleButton();
             this.txtDate = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.btnSubmit = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fpSpread1_Sheet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtDate.Properties)).BeginInit();
@@ -42,12 +44,13 @@
             // fpSpread1
             // 
             this.fpSpread1.AccessibleDescription = "fpSpread1, Sheet1, Row 0, Column 0, ";
-            this.fpSpread1.Location = new System.Drawing.Point(12, 37);
+            this.fpSpread1.Location = new System.Drawing.Point(11, 37);
             this.fpSpread1.Name = "fpSpread1";
             this.fpSpread1.Sheets.AddRange(new FarPoint.Win.Spread.SheetView[] {
             this.fpSpread1_Sheet1});
-            this.fpSpread1.Size = new System.Drawing.Size(1273, 352);
+            this.fpSpread1.Size = new System.Drawing.Size(1360, 636);
             this.fpSpread1.TabIndex = 0;
+            this.fpSpread1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fpSpread1_KeyDown);
             // 
             // fpSpread1_Sheet1
             // 
@@ -55,8 +58,9 @@
             fpSpread1_Sheet1.SheetName = "Sheet1";
             // Formulas and custom names must be loaded with R1C1 reference style
             this.fpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1;
-            fpSpread1_Sheet1.ColumnCount = 13;
+            fpSpread1_Sheet1.ColumnCount = 15;
             fpSpread1_Sheet1.RowCount = 50;
+            this.fpSpread1_Sheet1.Cells.Get(0, 0).Locked = false;
             this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 0).Value = "เลขที่บิล";
             this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 1).Value = "โควต้า";
             this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 2).Value = "ชื่อ-สกุล";
@@ -70,68 +74,85 @@
             this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 10).Value = "ราคาเหมาคีบ";
             this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 11).Value = "รหัสผู้รับเหมารวม";
             this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 12).Value = "ราคาเหมารวม";
+            this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 13).Value = "ประเภทอ้อย";
+            this.fpSpread1_Sheet1.ColumnHeader.Cells.Get(0, 14).Value = "ชั่งรวม";
             this.fpSpread1_Sheet1.ColumnHeader.Rows.Get(0).Height = 30F;
+            this.fpSpread1_Sheet1.Columns.Get(0).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(0).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(0).Label = "เลขที่บิล";
             this.fpSpread1_Sheet1.Columns.Get(0).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(0).Width = 66F;
+            this.fpSpread1_Sheet1.Columns.Get(1).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(1).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(1).Label = "โควต้า";
             this.fpSpread1_Sheet1.Columns.Get(1).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(1).Width = 59F;
+            this.fpSpread1_Sheet1.Columns.Get(2).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(2).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(2).Label = "ชื่อ-สกุล";
             this.fpSpread1_Sheet1.Columns.Get(2).Locked = false;
             this.fpSpread1_Sheet1.Columns.Get(2).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(2).Width = 117F;
+            this.fpSpread1_Sheet1.Columns.Get(3).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(3).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(3).Label = "ทะเบียนรถ";
             this.fpSpread1_Sheet1.Columns.Get(3).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(3).Width = 131F;
+            this.fpSpread1_Sheet1.Columns.Get(4).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(4).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(4).Label = "เลขที่คิว";
             this.fpSpread1_Sheet1.Columns.Get(4).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(4).Width = 77F;
+            this.fpSpread1_Sheet1.Columns.Get(5).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(5).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(5).Label = "รหัสผู้รับเหมาตัด";
             this.fpSpread1_Sheet1.Columns.Get(5).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(5).Width = 94F;
+            this.fpSpread1_Sheet1.Columns.Get(6).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(6).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(6).Label = "ราคาเหมาตัด";
             this.fpSpread1_Sheet1.Columns.Get(6).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(6).Width = 82F;
+            this.fpSpread1_Sheet1.Columns.Get(7).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(7).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(7).Label = "รหัสผู้รับเหมาบรรทุก";
             this.fpSpread1_Sheet1.Columns.Get(7).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(7).Width = 108F;
+            this.fpSpread1_Sheet1.Columns.Get(8).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(8).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(8).Label = "ราคาเหมาบรรทุก";
             this.fpSpread1_Sheet1.Columns.Get(8).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(8).Width = 100F;
+            this.fpSpread1_Sheet1.Columns.Get(9).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(9).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(9).Label = "รหัสผู้รับเหมาคีบ";
             this.fpSpread1_Sheet1.Columns.Get(9).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(9).Width = 101F;
+            this.fpSpread1_Sheet1.Columns.Get(10).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(10).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(10).Label = "ราคาเหมาคีบ";
             this.fpSpread1_Sheet1.Columns.Get(10).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(10).Width = 79F;
+            this.fpSpread1_Sheet1.Columns.Get(11).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(11).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(11).Label = "รหัสผู้รับเหมารวม";
             this.fpSpread1_Sheet1.Columns.Get(11).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(11).Width = 111F;
+            this.fpSpread1_Sheet1.Columns.Get(12).Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.fpSpread1_Sheet1.Columns.Get(12).HorizontalAlignment = FarPoint.Win.Spread.CellHorizontalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(12).Label = "ราคาเหมารวม";
             this.fpSpread1_Sheet1.Columns.Get(12).VerticalAlignment = FarPoint.Win.Spread.CellVerticalAlignment.Center;
             this.fpSpread1_Sheet1.Columns.Get(12).Width = 89F;
+            this.fpSpread1_Sheet1.Columns.Get(13).Label = "ประเภทอ้อย";
+            this.fpSpread1_Sheet1.Columns.Get(13).Width = 89F;
             this.fpSpread1_Sheet1.RowHeader.Columns.Default.Resizable = false;
             this.fpSpread1_Sheet1.RowHeader.Columns.Get(0).Width = 36F;
             this.fpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1;
             // 
             // btnSync
             // 
-            this.btnSync.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
-            this.btnSync.Location = new System.Drawing.Point(12, 395);
+            this.btnSync.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnSync.ImageOptions.Image")));
+            this.btnSync.Location = new System.Drawing.Point(12, 679);
             this.btnSync.Name = "btnSync";
             this.btnSync.Size = new System.Drawing.Size(106, 35);
             this.btnSync.TabIndex = 1;
@@ -141,24 +162,46 @@
             // txtDate
             // 
             this.txtDate.Enabled = false;
-            this.txtDate.Location = new System.Drawing.Point(1151, 11);
+            this.txtDate.Location = new System.Drawing.Point(1239, 10);
             this.txtDate.Name = "txtDate";
             this.txtDate.Size = new System.Drawing.Size(132, 20);
             this.txtDate.TabIndex = 2;
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(1115, 14);
+            this.labelControl1.Location = new System.Drawing.Point(1203, 13);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(30, 13);
             this.labelControl1.TabIndex = 3;
             this.labelControl1.Text = "วันที่ : ";
             // 
+            // btnSubmit
+            // 
+            this.btnSubmit.ImageOptions.ImageUri.Uri = "Apply";
+            this.btnSubmit.Location = new System.Drawing.Point(124, 679);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(104, 35);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "บันทึกข้อมูล";
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(11, 8);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(191, 19);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "โปรแกรมแก้ไขข้อมูลใบนำตัด";
+            // 
             // EditCutdoc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1295, 594);
+            this.ClientSize = new System.Drawing.Size(1379, 726);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.txtDate);
             this.Controls.Add(this.btnSync);
@@ -166,7 +209,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "EditCutdoc";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "โปรแกรมแก้ไขข้อมูลใบนำตัด";
+            this.Text = " ";
             this.Load += new System.EventHandler(this.EditCutdoc_Load);
             ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fpSpread1_Sheet1)).EndInit();
@@ -183,5 +226,7 @@
         private DevExpress.XtraEditors.SimpleButton btnSync;
         private DevExpress.XtraEditors.TextEdit txtDate;
         private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnSubmit;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
     }
 }
--- Com_AdminCutdoc/EditCutdoc.cs
+++ Com_AdminCutdoc/EditCutdoc.cs
@@ -24,8 +24,125 @@ namespace Com_AdminCutdoc
 
         private void btnSync_Click(object sender, EventArgs e)
         {
-            DataTable DT = new DataTable();
+            for (int i = 0; i < fpSpread1.ActiveSheet.Rows.Count; i++)
+            {
+                if (fpSpread1.ActiveSheet.Cells[i, 0].Text == "")
+                {
+                    break;
+                }
 
+                string lvBillNo = "0" + fpSpread1.ActiveSheet.Cells[i, 0].Text;
+                DataTable DT = new DataTable();
+                string lvSQL = "Select * From Queue_Diary INNER JOIN Cane_QueueData ON Queue_Diary.Q_Cutdoc = Cane_QueueData.C_ID INNER JOIN Cane_Quota ON Queue_Diary.Q_Quota = Cane_Quota.Q_ID INNER JOIN Cane_CaneType ON Cane_CaneType.C_ID = Queue_Diary.Q_CaneType WHERE Q_BillingNo LIKE '%" + lvBillNo + "%' AND Q_Year = '' ";
+                DT = GsysSQL.fncGetQueryData(lvSQL, DT);
+
+                for (int j = 0; j < DT.Rows.Count; j++)
+                {
+                    fpSpread1.ActiveSheet.Cells[i, 0].Text = DT.Rows[j]["Q_BillingNo"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 1].Text = DT.Rows[j]["Q_Quota"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 2].Text = DT.Rows[j]["Q_FirstName"].ToString() + " " + DT.Rows[j]["Q_LastName"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 3].Text = DT.Rows[j]["Q_CarNum"].ToString();
+                    if (DT.Rows[j]["Q_CarNum2"].ToString() != "")
+                    {
+                        fpSpread1.ActiveSheet.Cells[i, 3].Text += "/" + DT.Rows[j]["Q_CarNum2"].ToString();
+                    }
+                    fpSpread1.ActiveSheet.Cells[i, 4].Text = DT.Rows[j]["Q_No"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 5].Text = DT.Rows[j]["C_CutContactorId"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 6].Text = DT.Rows[j]["C_Price"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 7].Text = DT.Rows[j]["C_ContractorId"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 8].Text = DT.Rows[j]["C_TruckPrice"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 9].Text = DT.Rows[j]["C_KeebContractorId"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 10].Text = DT.Rows[j]["C_KeebContractorPrice"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 11].Text = DT.Rows[j]["C_AllContractor"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 12].Text = DT.Rows[j]["C_AllPrice"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 13].Text = DT.Rows[j]["C_Name"].ToString();
+                    fpSpread1.ActiveSheet.Cells[i, 14].Text = DT.Rows[j]["Q_WeightAllStatus"].ToString();
+                }
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int lvNumrow = fpSpread1.ActiveSheet.Rows.Count;
+            int i = 0;
+
+            for (i = 0; i < lvNumrow; i++)
+            {
+                this.Cursor = Cursors.WaitCursor;
+                if (fpSpread1.ActiveSheet.Cells[i, 0].Text == "")
+                {
+                    break;
+                }
+
+                //เก็บข้อมูล
+                string lvBillingNo = fpSpread1.ActiveSheet.Cells[i, 0].Text;
+                string lvQuota = fpSpread1.ActiveSheet.Cells[i, 1].Text;
+                string lvQNo = fpSpread1.ActiveSheet.Cells[i, 4].Text;
+                string lvCutContactorId = fpSpread1.ActiveSheet.Cells[i, 5].Text;
+                string lvCutPrice = fpSpread1.ActiveSheet.Cells[i, 6].Text;
+                string lvTruckContractor = fpSpread1.ActiveSheet.Cells[i, 7].Text;
+                string lvTruckPrice = fpSpread1.ActiveSheet.Cells[i, 8].Text;
+                string lvKeebContractorId = fpSpread1.ActiveSheet.Cells[i, 9].Text;
+                string lvKeebPrice = fpSpread1.ActiveSheet.Cells[i, 10].Text;
+                string lvAllContractor = fpSpread1.ActiveSheet.Cells[i, 11].Text;
+                string lvAllPrice = fpSpread1.ActiveSheet.Cells[i, 12].Text;
+                string lvWeightAllstatus = fpSpread1.ActiveSheet.Cells[i, 13].Text;
+                string lvBillIn = "0" + lvBillingNo;
+
+                string lvCarryPriceStatus = "";
+                string lvPayStauts = "";
+                if (lvTruckPrice != "")
+                {
+                    lvCarryPriceStatus = "1";
+                    lvPayStauts = "ชำระ";
+                }
+                else
+                {
+                    lvCarryPriceStatus = "";
+                    lvPayStauts = "ไม่ชำระ";
+                }
+
+                string lvQNo2 = lvQNo + ".1"; //คิวลูก
+
+                if(lvBillingNo != "")
+                {
+                    //บันทึกลงตาราง Queue_Online
+                    string lvSQL = "Update Cane_QueueData SET C_CutContactorId = '" + lvCutContactorId + "', C_Price = '" + lvCutPrice + "', C_ContractorId = '" + lvTruckContractor + "', " +
+                        "C_TruckPrice = '" + lvTruckPrice + "', C_KeebContractorId = '" + lvKeebContractorId + "', C_KeebContractorPrice = '" + lvKeebPrice + "', C_AllContractor = '" + lvAllContractor + "', " +
+                        "C_AllPrice = '" + lvAllPrice + "', C_PayStatus = '" + lvPayStauts + "' WHERE C_Queue = '" + lvQNo + "' ";
+                    string lvResult = GsysSQL.fncExecuteQueryData(lvSQL);
+
+                    //บันทึกลงตาราง Queue_Diary ตัวแม่
+                    lvSQL = "Update Queue_Diary SET Q_CutCar = '" + lvCutContactorId + "', Q_CutPrice = '" + lvCutPrice + "', Q_CarryPrice = '" + lvTruckPrice + "', Q_CarryPriceStatus = '" + lvCarryPriceStatus + "' " +
+                        "WHERE Q_No = '" + lvQNo + "' AND Q_Year = '' ";
+                    lvResult = GsysSQL.fncExecuteQueryData(lvSQL);
+                }
+
+                if(lvQNo2 != "" && lvWeightAllstatus == "1")
+                {
+                    //บันทึกลงตาราง Queue_Diary ตัวลูก
+                    string lvSQL = "Update Queue_Diary SET Q_CutCar = '" + lvCutContactorId + "', Q_CutPrice = '" + lvCutPrice + "', Q_CarryPrice = '" + lvTruckPrice + "', Q_CarryPriceStatus = '" + lvCarryPriceStatus + "' " +
+                        "WHERE Q_No = '" + lvQNo2 + "' And Q_Year = '' ";
+                    string lvResult = GsysSQL.fncExecuteQueryData(lvSQL);
+                }
+                else
+                {
+                    //ไม่ต้องทำอะไร
+                }
+            }
+
+            MessageBox.Show("บันทึกข้อมูล : " + i + " บรรทัด สำเร็จ...", "แจ้งเตือน!..", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Cursor = Cursors.Default;
+        }
+
+        private void fpSpread1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                FarPoint.Win.Spread.Model.CellRange cr;
+                cr = fpSpread1.ActiveSheet.GetSelection(0);
+                fpSpread1.ActiveSheet.ClearRange(cr.Row, cr.Column, cr.RowCount, cr.ColumnCount, true);
+            }
         }
     }
 }
